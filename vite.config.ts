608db80a9ecd637e10b4aa76a170a8e1196d85a3
Maximo Mussini <maximomussini@gmail.com>
2021-03-05 12:47:26 -0300

    Upgrade vite-plugin-windicss (version that added root)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 
 import { createVuePlugin as Vue2Plugin } from 'vite-plugin-vue2'
 import RubyPlugin from 'vite-plugin-ruby'
 import WindiCSS from 'vite-plugin-windicss'
-
-const resolve = (file) => path.resolve(process.cwd(), file)
 
 export default defineConfig({
   plugins: [
@@ -13,10 +10,9 @@ export default defineConfig({
     Vue2Plugin(),
     WindiCSS({
       root: process.cwd(),
-      config: resolve('windi.config.ts'),
       scan: {
         fileExtensions: ['erb', 'html', 'vue', 'rb', 'jsx', 'tsx'],
-        dirs: ['app/views', 'app/helpers', 'app/javascript'].map(resolve),
+        dirs: ['app/views', 'app/helpers', 'app/javascript'],
       },
     }),
   ],
