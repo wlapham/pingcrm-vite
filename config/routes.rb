44e972d3092497fedef0fcdcdbe769a8f44dbe63
Maximo Mussini <maximomussini@gmail.com>
2021-03-16 17:02:09 -0300

    refactor: Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,28 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  devise_for :users, skip: [ :sessions, :passwords, :registrations ]
-  as :user do
-    get 'login', to: 'users/sessions#new', as: :new_user_session, export: :true
-    post 'login', to: 'users/sessions#create', as: :user_session, export: :true
-    match 'logout', to: 'users/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via, export: :true
-  end
-
-  resources :reports, only: [ :index ], export: :true
-  resources :users, except: [ :show ], export: :true do
-    member do
-      put 'restore'
+  defaults export: true do
+    devise_for :users, skip: [:sessions, :passwords, :registrations]
+    as :user do
+      get 'login', to: 'users/sessions#new', as: :new_user_session
+      post 'login', to: 'users/sessions#create', as: :user_session
+      match 'logout', to: 'users/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
     end
-  end
-  resources :organizations, except: [ :show, :new ], export: :true do
-    member do
-      put 'restore'
-    end
-  end
-  resources :contacts, except: [ :update, :show, :destroy ], export: :true do
-    member do
-      delete :destroy, export: true
-      patch :update, export: true
-      put :restore, export: true
-    end
-  end
 
-  get '/error-500' => 'dashboard#example_exception', export: :true
+    resources :reports, only: [:index]
 
-  root 'dashboard#index', export: :true
+    resources :users, except: [:show] do
+      put :restore, on: :member
+    end
+
+    resources :organizations, except: [:show, :new] do
+      put :restore, on: :member
+    end
+
+    resources :contacts, except: [:show] do
+      put :restore, on: :member
+    end
+
+    get '/error-500' => 'dashboard#example_exception'
+
+    root 'dashboard#index'
+  end
 end
