5d2bd89a17aef818865f9335119df4857c5fb4fb
Maximo Mussini <maximomussini@gmail.com>
2022-05-13 00:26:26 -0300

    chore: mention the app is ssr-enabled

--- app/javascript/Pages/Dashboard/Index.vue
+++ app/javascript/Pages/Dashboard/Index.vue
@@ -3,7 +3,7 @@
     <h1 class="mb-8 font-bold text-3xl">
       Dashboard
     </h1>
-    <p class="mb-8 leading-normal">
+    <p class="mb-4 leading-loose">
       Hey there! Welcome to
       <ExternalLink href="https://github.com/ElMassimo/pingcrm-vite">
         Ping CRM
@@ -16,30 +16,25 @@
         Ruby on Rails
       </ExternalLink>.
     </p>
-    <p class="mb-8 leading-normal">
+    <p class="mb-4 leading-loose">
       This demo is running on
-      <ExternalLink href="https://vite-rails.netlify.app/">
-        Vite ⚡️ Rails
-      </ExternalLink>
+      <ExternalLink href="https://vite-rails.netlify.app/">Vite ⚡️ Rails</ExternalLink>
       instead of Webpacker, which provides a
-      <ExternalLink href="https://vite-rails.netlify.app/guide/introduction">
-        faster development experience
-      </ExternalLink>
+      <ExternalLink href="https://vite-rails.netlify.app/guide/introduction">faster development experience</ExternalLink>
       thanks to
-      <ExternalLink href="https://vite-rails.netlify.app/guide/introduction">
-        Vite
-      </ExternalLink>.
-    </p>
-
-    <p class="mb-8 leading-normal">
+      <ExternalLink href="https://vite-rails.netlify.app/guide/introduction">Vite</ExternalLink>.
+      <br/>
       It also uses
       <ExternalLink href="https://js-from-routes.netlify.app/">
         JS From Routes
       </ExternalLink>
       to simplify navigation and making requests to the Rails server without hardcoding paths.
     </p>
+    <p class="mb-4 leading-loose">
+      Pages are server-side rendered thanks to the SSR support provided by both <ExternalLink href="https://inertiajs.com/server-side-rendering">Inertia</ExternalLink> and <ExternalLink href="https://vite-rails.netlify.app/config/#ssr-options-experimental">Vite Ruby</ExternalLink>. Try disabling JS!
+    </p>
 
-    <p class="mb-10 leading-normal">
+    <p class="mb-14 leading-loose">
       Version
       <a :href="releaseUrl" class="hover:underline">{{ release.commit || 'development' }}</a>
       &minus;
